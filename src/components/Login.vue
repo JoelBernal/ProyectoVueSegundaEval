193c2a4fddf960717a08dd62c34894545dafa094
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2023-03-08 20:52:24 +0100

    Modificado login/register form

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,44 +1,142 @@
 <template>
-  <div>
-    <!-- <Navbar/> -->
-    <v-app id="inspire">
-      <v-content>
-        <v-container fluid fill-height>
-          <v-layout align-center justify-center>
-            <v-flex xs12 sm8 md4>
-              <v-card class="elevation-12">
-                <v-toolbar dark color="primary">
-                  <v-toolbar-title>Login form</v-toolbar-title>
-                </v-toolbar>
-                <v-card-text>
-                  <v-form>
-                    <v-text-field name="login" label="Usuario" type="text"></v-text-field>
-                    <v-text-field id="password" name="Contraseña" label="Password" type="password"></v-text-field>
-                  </v-form>
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="primary" to="/">Login</v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-content>
-    </v-app>
-  </div>
+  <v-app id="inspire">
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="8">
+            <v-card class="elevation-12">
+              <v-window v-model="step">
+                <v-window-item :value="1">
+                  <v-row>
+                    <v-col cols="12" md="8">
+                      <v-card-text class="mt-12">
+                        <h1
+                          class="text-center display-2 teal--text text--accent-3"
+                        >Iniciar Sesion en Librerias Paco</h1>
+                        <div class="text-center mt-4">
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon >fab fa-facebook-f</v-icon>
+                          </v-btn>
+
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-google-plus-g</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-linkedin-in</v-icon>
+                          </v-btn>
+                        </div>
+                        <v-form>
+                          <v-text-field
+                            label="Email"
+                            name="Email"
+
+                            type="text"
+                            color="teal accent-3"
+                          />
+
+                          <v-text-field
+                            id="password"
+                            label="Password"
+                            name="password"
+                            type="password"
+                            color="teal accent-3"
+                          />
+                        </v-form>
+                        <h3 class="text-center mt-4">Has olvidado la contraseña ?</h3>
+                      </v-card-text>
+                      <div class="text-center mt-3">
+                        <v-btn style="margin-bottom: 20px" rounded color="teal accent-3" to="/home" dark>Iniciar Sesion</v-btn>
+                      </div>
+                    </v-col>
+                    <v-col cols="12" md="4" class="teal accent-3">
+                      <v-card-text class="white--text mt-12">
+                        <h1 class="text-center display-1">Hola, Amigo!</h1>
+                        <h5
+                          class="text-center"
+                        >Introduce tus datos personales y empieza a trabajar con nosotros</h5>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn rounded outlined dark @click="step++">REGISTRARSE</v-btn>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+                <v-window-item :value="2">
+                  <v-row class="fill-height">
+                    <v-col cols="12" md="4" class="teal accent-3">
+                      <v-card-text class="white--text mt-12">
+                        <h1 class="text-center display-1">Bienvenido de nuevo!</h1>
+                        <h5
+                          class="text-center"
+                        >Para estar conectado con nosotros por favor, entra con tu informacion personal</h5>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn rounded outlined dark @click="step--">INICIAR SESION</v-btn>
+                      </div>
+                    </v-col>
+
+                    <v-col cols="12" md="8">
+                      <v-card-text class="mt-12">
+                        <h1 class="text-center display-2 teal--text text--accent-3">Crear cuenta</h1>
+                        <div class="text-center mt-4">
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-facebook-f</v-icon>
+                          </v-btn>
+
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-google-plus-g</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-linkedin-in</v-icon>
+                          </v-btn>
+                        </div>
+                        <v-form>
+                          <v-text-field
+                            label="Name"
+                            name="Name"
+                            type="text"
+                            color="teal accent-3"
+                          />
+                          <v-text-field
+                            label="Email"
+                            name="Email"
+                            type="text"
+                            color="teal accent-3"
+                          />
+
+                          <v-text-field
+                            id="password"
+                            label="Password"
+                            name="password"
+                            type="password"
+                            color="teal accent-3"
+                          />
+                        </v-form>
+                      </v-card-text>
+                      <div class="text-center mt-n5">
+                        <v-btn style="margin-bottom: 20px" rounded color="teal accent-3" to="/home" dark>Registrarse</v-btn>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+              </v-window>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
- 
+
 <script>
 export default {
-  name: "Login",
-  components: {
-    // Navbar
-  },
+  data: () => ({
+    step: 1,
+    
+  }),
   props: {
-    source: String,
-  },
+    source: String
+  }
 };
 </script>
- 
-<style></style>
+
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import '@fortawesome/fontawesome-free/css/all.css'
+import Vuetify from 'vuetify/lib'
+
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-});
-
+    icons: {
+      iconfont: 'fa', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+    },
+  })
